e79a51ec77b065d129d099601a6f958dd0d75042
brewchetta <chett.tiller@gmail.com>
2026-02-11 13:04:18 -0500

    up to victory screen

--- index.js
+++ index.js
@@ -8,10 +8,40 @@ let guessedLetters = []
 let currentWord = null
 let guessesRemaining = 6
 
+let currentScore = 0
+
 // shows guessed letters and remaining spaces
 const wordArea = document.querySelector("#word-area") 
-
+// filled with buttons for keyboard
 const lettersKeyboard = document.querySelector("#letters-keyboard")
+// progressive hangman img
+const hangmanImg = document.querySelector("#hangman-img")
+// displays guessedLetters
+const guessedLettersArea = document.querySelector("#guessed-letters-area")
+// displays current score
+const scoreArea = document.querySelector("#score-area")
+// popup for victory
+const victoryModal = document.querySelector("#victory-modal")
+
+// urls for different stages of hangman imgs
+const hangmanImg01 = "https://www.oligalma.com/downloads/images/hangman/hangman/4.jpg"
+const hangmanImg02 = "https://www.oligalma.com/downloads/images/hangman/hangman/5.jpg"
+const hangmanImg03 = "https://www.oligalma.com/downloads/images/hangman/hangman/6.jpg"
+const hangmanImg04 = "https://www.oligalma.com/downloads/images/hangman/hangman/7.jpg"
+const hangmanImg05 = "https://www.oligalma.com/downloads/images/hangman/hangman/8.jpg"
+const hangmanImg06 = "https://www.oligalma.com/downloads/images/hangman/hangman/9.jpg"
+const hangmanImg07 = "https://www.oligalma.com/downloads/images/hangman/hangman/10.jpg"
+
+const hangmanImageSteps = {
+    6: hangmanImg01,
+    5: hangmanImg02,
+    4: hangmanImg03,
+    3: hangmanImg04,
+    2: hangmanImg05,
+    1: hangmanImg06,
+    0: hangmanImg07,
+}
+
 
 // FUNCTIONS
 
@@ -58,10 +88,15 @@ function handleClickLetterKey( event ) {
         return
     }
 
-    // add letter to guessed letters
+    // add letter to guessed letters if not currently guessed
     const chosenLetter = event.target.textContent
+    if ( guessedLetters.includes(chosenLetter) ) {
+        return
+    }
     guessedLetters.push(chosenLetter)
+    updateGuessedLettersArea(chosenLetter)
 
+    // handle correct or incorrect guess
     if ( currentWord.includes(chosenLetter) ) {
         handleCorrectGuess(chosenLetter)
     } else {
@@ -72,6 +107,8 @@ function handleClickLetterKey( event ) {
 // display letter in the correct spot in the #word-area
 // TODO: if the word area is filled in we move to victory screen
 function handleCorrectGuess(chosenLetter) {
+    // add to score
+    changeScore(10)
     // loop through the currentWord
     for (let i = 0; i < currentWord.length; i++) {
         // if letter maches currentWord letter
@@ -81,13 +118,51 @@ function handleCorrectGuess(chosenLetter) {
             span.textContent = chosenLetter
         }
     }
+    // check if the player has won
+    checkWinCondition()
 }
 
-// ????????????????????????????????????
+// decrement guessesLeft
+// changes hangman image to the next image
+// TODO: alert about incorrect guess
+// TODO: if no guesses left show the losing screen
 function handleIncorrectGuess(chosenLetter) {
-
+    // subtract from score
+    changeScore(-5)
+    // change guesses remaining and image
+    if (guessesRemaining > 0) {
+        guessesRemaining--
+        const nextImageURL = hangmanImageSteps[guessesRemaining]
+        hangmanImg.src = nextImageURL
+        hangmanImg.alt = `${guessesRemaining} guesses remaining`
+    }
 }
 
+
+function updateGuessedLettersArea(chosenLetter) {
+    const span = document.createElement("span")
+    span.textContent = chosenLetter
+    guessedLettersArea.append(span)
+}
+
+function changeScore(change) {
+    currentScore += change
+    scoreArea.textContent = currentScore
+}
+
+function checkWinCondition() {
+    for (let i = 0; i < currentWord.length; i++) {
+        const currentWordLetter = currentWord[i]
+        const isLetterIncluded = guessedLetters.includes( currentWordLetter )
+
+        // if letter NOT included then short circuit the function
+        if ( !isLetterIncluded ) {
+            return
+        }
+    }
+
+    victoryModal.style.display = "block"
+}
 
 // TESTING AREA
 initializeNewGame()
